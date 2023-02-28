83daaed0ef01a8f95a756c5dc7c246c69c0315ea
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-02-28 10:32:53 +0100

    useTasks import from useTasks.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
+import styled from "styled-components";
 
 const getInitialTasks = () => {
   const tasksFromLocalStorage = localStorage.getItem("tasks");
