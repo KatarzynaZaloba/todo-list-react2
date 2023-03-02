33fae90d056ea9201755def42038053307b4517c
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-03-02 17:33:30 +0100

    code formatted

--- src/features/tasks/TaskList/styled.js
+++ src/features/tasks/TaskList/styled.js
@@ -1,41 +1,41 @@
 import styled, { css } from "styled-components";
 
 export const List = styled.ul`
-    list-style: none;
-    padding: 0;
-    margin: 0;
+  list-style: none;
+  padding: 0;
+  margin: 0;
 `;
 
 export const Item = styled.li`
-    display:grid;
-    grid-template-columns: auto 1fr auto;
-    grid-gap: 10px;
-    align-items: center;
-    padding: 10px;
-    border-bottom: 1px solid ${({ theme }) => theme.color.alto};
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  grid-gap: 10px;
+  align-items: center;
+  padding: 10px;
+  border-bottom: 1px solid ${({ theme }) => theme.color.alto};
     ${({hidden}) => hidden && css`
-        display: none;
+      display: none;
     `}
 `;
 
 export const Content = styled.span`
     word-break: break-word;
     ${({done}) => done && css`
-        text-decoration: line-through;
+      text-decoration: line-through;
     `}
 `;
 
 export const Button = styled.button`
-    color: ${({theme }) => theme.color.white};
-    width: 30px;
-    height: 30px;
-    border: none;
-    padding: 0;
-    transition: filter 0.3s;
-    cursor: pointer;
+  color: ${({ theme }) => theme.color.white};
+  width: 30px;
+  height: 30px;
+  border: none;
+  padding: 0;
+  transition: filter 0.3s;
+  cursor: pointer;
 `;
 
-  export const ToggleDoneButton = styled(Button)`
+export const ToggleDoneButton = styled(Button)`
           background: ${({ theme }) => theme.color.forestGreen};
           &:hover{
              filter: brightness(110%);
