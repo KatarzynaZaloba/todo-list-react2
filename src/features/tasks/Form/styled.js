1d5b303955051e5450ca29afefe1c1e1c9cec864
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-03-02 17:36:44 +0100

    media query added, code formatted

--- src/features/tasks/Form/styled.js
+++ src/features/tasks/Form/styled.js
@@ -1,32 +1,34 @@
 import styled from "styled-components";
 
 export const StyledForm = styled.form`
-    display: grid;
-    grid-template-columns: 1fr auto;
-    grid-gap: 20px;
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        grid-template-columns: 1fr;
-    }
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-gap: 20px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    grid-template-columns: 1fr;
+    display: flex;
+    flex-wrap: wrap;
+  }
 `;
 
-export const Input = styled.input`
-    padding: 10px;
-    border: 1px solid ${({ theme }) => theme.color.alto};
+export const StyledInput = styled.input`
+  border: 2px solid ${({ theme }) => theme.color.gallery};
+  padding: 10px;
+  width: 100%;
 `;
 
 export const Button = styled.button`
-    background: ${({ theme }) => theme.color.teal};
-    color: ${({ theme }) => theme.color.white};
-    border: none;
-    padding: 10px;
-    transition: 0.3s;
-    padding: 10px;
-    &:hover{
-     filter: brightness(110%);
-     cursor: pointer;
-     transform: scale(1.1); 
-    }
-    &:active{
-     filter: brightness(120%); 
-    }
+  padding: 10px;
+  background-color: ${({ theme }) => theme.color.teal};
+  color: ${({ theme }) => theme.color.white};
+  border: none;
+  cursor: pointer;
+  transition: 1s;
+  &:hover {
+    filter: brightness(130%);
+    transform: scale(1.2);
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    width: 100%;
+  }
 `;
