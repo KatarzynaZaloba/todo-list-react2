a507aec06a51555294fec87a1e11950ea5c3f838
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-03-02 17:33:03 +0100

    Provider added

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '../src/App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 import { theme } from "./theme";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
