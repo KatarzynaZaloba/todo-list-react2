8d8735055855f0e278fa208e4176c629b816cd2e
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-03-02 17:42:21 +0100

    changed Wrapper into StyledSection

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -1,30 +1,30 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.section`
-    background: ${({ theme }) => theme.color.white};
-    margin-bottom: 10px;
-    box-shadow: 0 0 5px ${({ theme }) => theme.color.alto};
+export const StyledSection = styled.section`
+  margin: 10px 0;
+  background-color: white;
+  box-shadow: 0 0 5px #ddd;
+`;
+
+export const SectionBody = styled.div`
+  padding: 20px;
 `;
 
 export const Header = styled.header`
-    border-bottom: 1px solid ${({ theme }) => theme.color.alto};
-    display: grid;
-    grid-template-columns: 1fr auto;
-    border-bottom: 1px solid #ddd ;
-    align-item: center;
-    
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        grid-template-columns: 1fr;
-        padding: 10px;
-    }
+  border-bottom: 1px solid ${({ theme }) => theme.color.gallery};
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-gap: 20px;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 20px;
+  padding: 20px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const Title = styled.h2`
-    margin: 0;
-    font-size: 20px;
-    padding: 20px;
-`;
-
-export const Body = styled.div`
-    padding: 20px;
+  font-size: 20px;
+  margin: 0;
 `;
