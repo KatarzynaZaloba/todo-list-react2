bf9abc548444a36bbc3f8dabe04ff9fef5dd5a6b
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-03-02 17:42:45 +0100

    created taskSlice

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,47 +1,26 @@
-import Form from "../tasks/Form";
+import Form from "./Form";
 import TaskList from "./TaskList";
 import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
-import { Container } from "../../common/Container/styled";
-import { useTasks } from "../../useTasks";
+import Container from "../../common/Container";
 
 function Tasks() {
-  const {
-    hideDone,
-    tasks,
-    toggleHideDone,
-    removeTask,
-    toggleTaskDone,
-    setAllDone,
-    addNewTask,
-  } = useTasks();
-
   return (
     <Container>
       <Header title="Lista zadań" />
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form addNewTask={addNewTask} />}
+        body={<Form />}
       />
 
       <Section
         title="Lista zadań"
         body={
-          <TaskList
-            tasks={tasks}
-            hideDone={hideDone}
-            removeTask={removeTask}
-            toggleTaskDone={toggleTaskDone}
-          />
+          <TaskList />
         }
         extraHeaderContent={
-          <Buttons
-            tasks={tasks}
-            hideDone={hideDone}
-            toggleHideDone={toggleHideDone}
-            setAllDone={setAllDone}
-          />
+          <Buttons />
         }
       />
     </Container>
